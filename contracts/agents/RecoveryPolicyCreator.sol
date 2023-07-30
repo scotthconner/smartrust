fdd6d63975a6a83582363729f5c8acf817e04743
scotthconner <scotthconner@gmail.com>
2023-07-29 20:56:36 -0500

    Rough cut of the final event + recovery orchestrator

--- contracts/agents/RecoveryPolicyCreator.sol
+++ contracts/agents/RecoveryPolicyCreator.sol
@@ -25,8 +25,10 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 // We need the Locksmith ABI to create trusts 
 import '../interfaces/ILocksmith.sol';
 import '../interfaces/IKeyVault.sol';
+import '../interfaces/INotary.sol';
 import '../interfaces/IKeyOracle.sol';
 import '../interfaces/IAlarmClock.sol';
+import '../interfaces/ITrustEventLog.sol';
 import '../interfaces/ITrustRecoveryCenter.sol';
 
 ///////////////////////////////////////////////////////////
@@ -44,21 +46,37 @@ contract RecoveryPolicyCreator is ERC1155Holder, Initializable, OwnableUpgradeab
     ///////////////////////////////////////////////////////
     // Data Structures 
     ///////////////////////////////////////////////////////
+    struct DeadmanConfiguration {
+        bytes32 description;
+        uint256 alarmTime;
+        uint256 snoozeInterval;
+        uint256 snoozeKeyId;
+    }
+
     struct KeyOracleConfiguration {
         bytes32 description;
         uint256 keyId;
     }
 
     struct RecoveryPolicyConfiguration {
+        // notary requirements:
+        // by default, these dispatchers were not added
+        // to the account on trust creation. So we want to take signal
+        // from the key holder if they need it or not.
+        // this saves gas on trust creation, and avoids
+        // spending gas onchain figuring it out during this
+        // action. if the user is wrong, the transaction will fail.
+        // users can use the notary interface to find out if
+        // they need the notary permission easier in javascript
+        // on the client
+        bool needAlarmNotary;
+        bool needOracleNotary;
+
         // guardians, addresses that are used for recovery
         address[] guardians;
 
         // deadman switch configuration
-        bool    useDeadman;
-        bytes32 deadmanDescription;
-        uint256 alarmTime;
-        uint256 snoozeInterval;
-        uint256 snoozeKeyId;
+        DeadmanConfiguration[] deadmen;
 
         // key oracle configurations
         KeyOracleConfiguration[] keyOracles;
@@ -67,9 +85,11 @@ contract RecoveryPolicyCreator is ERC1155Holder, Initializable, OwnableUpgradeab
     ///////////////////////////////////////////////////////
     // Storage
     ///////////////////////////////////////////////////////
-    ILocksmith  public locksmith;
-    IAlarmClock public alarmClock;
-    IKeyOracle  public keyOracle;
+    ILocksmith     public locksmith;
+    IAlarmClock    public alarmClock;
+    IKeyOracle     public keyOracle;
+    INotary        public notary;
+    ITrustEventLog public trustEventLog;
     ITrustRecoveryCenter public trustRecoveryCenter;
 
     ///////////////////////////////////////////////////////
@@ -90,13 +110,15 @@ contract RecoveryPolicyCreator is ERC1155Holder, Initializable, OwnableUpgradeab
      * Fundamentally replaces the constructor for an upgradeable contract.
      *
      */
-    function initialize(address _Locksmith, address _AlarmClock, address _KeyOracle, address _TrustRecoveryCenter) initializer public {
+    function initialize(address _Locksmith, address _Notary, address _AlarmClock, address _KeyOracle, address _TrustRecoveryCenter, address _TrustEventLog) initializer public {
         __Ownable_init();
         __UUPSUpgradeable_init();
         locksmith  = ILocksmith(_Locksmith);
         alarmClock = IAlarmClock(_AlarmClock);
         keyOracle  = IKeyOracle(_KeyOracle);
         trustRecoveryCenter = ITrustRecoveryCenter(_TrustRecoveryCenter);
+        trustEventLog = ITrustEventLog(_TrustEventLog);
+        notary = INotary(_Notary);
     }
 
     /**
@@ -132,8 +154,6 @@ contract RecoveryPolicyCreator is ERC1155Holder, Initializable, OwnableUpgradeab
     function onERC1155Received(address, address from, uint256 keyId, uint256 count, bytes memory data)
         public virtual override returns (bytes4) {
 
-        bytes32 deadmanHash;
-
         // make sure the count is exactly 1 of whatever it is.
         require(count == 1, 'IMPROPER_KEY_INPUT');
 
@@ -144,29 +164,75 @@ contract RecoveryPolicyCreator is ERC1155Holder, Initializable, OwnableUpgradeab
         // grab the encoded information
         (RecoveryPolicyConfiguration memory config) 
             = abi.decode(data, (RecoveryPolicyConfiguration));
+        
+        bytes32[] memory events = new bytes32[](config.deadmen.length + config.keyOracles.length);
+        uint256 eventCount = 0;
 
-        // at this point we just assume the key has the permissions to create
-        // events, if it doesn't it will revert
-
-        // create events as needed 
-        if (config.useDeadman) {
-            deadmanHash = alarmClock.createAlarm(keyId, 
-                config.deadmanDescription,
-                config.alarmTime,
-                config.snoozeInterval,
-                config.snoozeKeyId);
+        // ensure we have notary access for the alarm clock deadman
+        if(config.needAlarmNotary) {
+            // this will fail if the key isn't root
+            notary.setTrustedLedgerRole(keyId, 2, address(trustEventLog), address(alarmClock),
+                true, stringToBytes32('Alarm Clock'));
         }
+
+        // create each of the deadman switches
+        for(uint256 d = 0; d < config.deadmen.length; d++) {
+            events[eventCount++] = alarmClock.createAlarm(keyId, 
+                config.deadmen[d].description,
+                config.deadmen[d].alarmTime,
+                config.deadmen[d].snoozeInterval,
+                config.deadmen[d].snoozeKeyId);
+        }
+        
+        // ensure we have notary access for the key oracle 
+        if(config.needOracleNotary) {
+            // this will fail if the key isn't root
+            notary.setTrustedLedgerRole(keyId, 2, address(trustEventLog), address(keyOracle),
+                true, stringToBytes32('Key Oracle'));
+        }
+
+        // create each of the key oracles
         for(uint256 x = 0; x < config.keyOracles.length; x++) {
-            
+            events[eventCount++]= keyOracle.createKeyOracle(keyId, config.keyOracles[x].keyId,
+                config.keyOracles[x].description);
         }
 
         // create the policy
-        //IERC1155(msg.sender).safeTransferFrom(address(this), keyAddressFactory, keyId, 1,
-        //    abi.encode(newKeyId, provider));
+        IERC1155(msg.sender).safeTransferFrom(address(this), address(trustRecoveryCenter), keyId, 1,
+            abi.encode(config.guardians, events));
 
         // send the root key back
         IERC1155(msg.sender).safeTransferFrom(address(this), from, keyId, 1, ""); 
 
+        // make sure we aren't holding any keys
+        assert(IERC1155(msg.sender).balanceOf(address(this), keyId) == 0);
+
         return this.onERC1155Received.selector;
+    }
+
+    /**
+     * stringToBytes32
+     *
+     * Normally, the user is providing a string on the client side
+     * and this is done with javascript. The easiest way to solve
+     * this without creating more APIs on the contract and requiring
+     * more gas is to give credit to this guy on stack overflow.
+     *
+     * https://ethereum.stackexchange.com/questions/9142/how-to-convert-a-string-to-bytes32
+     *
+     * @param source the string you want to convert
+     * @return result the equivalent result of the same using ethers.js
+     */
+    function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
+        // Note: I'm not using this portion because there isn't
+        // a use case where this will be empty.
+        // bytes memory tempEmptyStringTest = bytes(source);
+        //if (tempEmptyStringTest.length == 0) {
+        //    return 0x0;
+        // }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
     }
 }
